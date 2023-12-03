ef2e9779d0c90d51f80630576655b99c0467438d
taldoflemis <coderflemis@gmail.com>
2023-12-03 12:55:22 -0300

    feat: adicionar os outros membros

--- frontend/src/app/about/page.tsx
+++ frontend/src/app/about/page.tsx
@@ -10,6 +10,33 @@ const members: MemberCardProps[] = [
     linkedin: "https://www.linkedin.com/in/said-rodrigues/",
     github: "https://github.com/talDoFlemis/",
   },
+  {
+    name: "Amarildo",
+    image:
+      "https://media.licdn.com/dms/image/C4E03AQHb0JgDxDdAOg/profile-displayphoto-shrink_200_200/0/1637526727593?e=1707350400&v=beta&t=O-ykmt6sFByrBIGjFc7LJo6Y74oqklxHGsMUCFws4JU",
+    linkedin: "https://www.linkedin.com/in/said-rodrigues/",
+    github: "https://github.com/amarildorfilho",
+  },
+  {
+    name: "Carlos",
+    image: "https://avatars.githubusercontent.com/u/67774642?v=4",
+    linkedin: "https://www.linkedin.com/in/ingenieco-cegu/",
+    github: "https://github.com/Ingenieco",
+  },
+  {
+    name: "Elvin",
+    image:
+      "https://media.licdn.com/dms/image/D4D03AQGCHaWk47MszA/profile-displayphoto-shrink_100_100/0/1681072351673?e=1707350400&v=beta&t=Yp95X4UQLNJMa2487FNoCZDDs_-_Lx2UqKYzvrzfgNA",
+    linkedin: "https://www.linkedin.com/in/elvinmatheus/",
+    github: "https://github.com/elvinmatheus",
+  },
+  {
+    name: "Luann",
+    image:
+      "https://media.licdn.com/dms/image/D4E35AQFTwWTDQ1o0QQ/profile-framedphoto-shrink_200_200/0/1643896258494?e=1702224000&v=beta&t=dlgxFYiPUipMTA-_9IPPl2snBHKBjoWcAjFOFjN7Umw",
+    linkedin: "https://www.linkedin.com/in/luann-alves-43937416b/",
+    github: "https://github.com/Lonalt",
+  },
 ];
 
 const About = () => {
