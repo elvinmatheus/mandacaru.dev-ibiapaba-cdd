f7f09efc93abd5150e68eea046d77becad0d067b
taldoflemis <coderflemis@gmail.com>
2023-12-03 12:48:07 -0300

    feat: adicionar novo texto ao the project

--- frontend/src/components/about/the-project.tsx
+++ frontend/src/components/about/the-project.tsx
@@ -1,14 +1,18 @@
 import React from "react";
-import { TypographyH1, TypographyP } from "../typography";
+import { TypographyH1 } from "../typography";
 
 const TheProject = () => {
   return (
     <div className="flex flex-col items-center justify-center">
       <TypographyH1>The Project</TypographyH1>
-      <TypographyP>
-        The project consists of a web application that uses Natural Language
-        Processing (NLP) to analyze the sentiment of the financial market.
-      </TypographyP>
+      <p className="text-md md:text-lg lg:text-xl text-gray-900 mb-8 text-center">
+        ACELS is a useful tool for financial sentiment analysis. It uses the
+        DecisionTreeClassifier algorithm from the Sci-kit Learn package to
+        identify whether the sentiment present in a text is positive or
+        negative. Our model has an accuracy of 85%. With ACELS, you can automate
+        financial sentiment analysis and optimize your data analysis processes.
+        Try it now!
+      </p>
     </div>
   );
 };
